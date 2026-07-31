4213273f67e5f1056d5fcc157cdfdcff7a47fdca
Jacob Lewis-Shores <jacoblewisshores@gmail.com>
2026-07-31 13:39:51 +0100

    Add backend setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
